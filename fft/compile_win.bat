e69d5b921ca9afd24c73a7ed22afc68bf63f4614
nakak <66908843+nakakq@users.noreply.github.com>
2021-08-21 04:27:39 +0900

    bug fix

--- fft/compile_win.bat
+++ fft/compile_win.bat
@@ -1,4 +1,4 @@
 : compile script for windows
 : run this on emsdk console
 @echo off
-emcc fftsg.c -O3 -s EXPORTED_FUNCTIONS="['_cdft','_rdft','_malloc','_free']" -o fftsg.js
+emcc fftsg.c -O3 -s WASM=1 -s ALLOW_TABLE_GROWTH=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_cdft','_rdft','_malloc','_free']" -o fftsg.js
